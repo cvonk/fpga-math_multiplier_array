83994b690411528ff2e6cc426e207d9de06f0f85
cvonk <coert.vonk@gmail.com>
2015-10-20 11:24:54 -0700

    finish renaming files

--- math_multiplier_array_tb.v
+++ math_multiplier_array_tb.v
@@ -1,5 +1,7 @@
 `timescale 1ns / 100ps
 `default_nettype none
+// (c) 2015, by Coert Vonk
+// http://www.coertvonk.com/technology/logic/fpga-math-verilog-12758/4
 
 module math_multiplier_array_tb;
 
@@ -20,5 +22,7 @@ module math_multiplier_array_tb;
          for ( b = 0; b < 2**N; b = b + 1 )
 			   #20 if ( a * b != p )
                 $display( "%d * %d != %d", a, b, p );
+		$display("EOT");
 	end
+	
 endmodule
